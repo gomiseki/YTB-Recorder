6d39b5e5ab2d09cd834edaa907b526d48ec4ec46
gomiseki <kimju1755@gmail.com>
2022-02-08 07:41:20 +0900

    [v1]

--- public/contentScript.js
+++ public/contentScript.js
@@ -23,6 +23,10 @@ let voiceRecorder = false;
 
 let audioContext = new AudioContext();
 
+//볼륨조절 변수들
+let instNode = audioContext.createGain();
+let voiceNode = audioContext.createGain();
+
 function blobToBase64(blob) {
   return new Promise((resolve, _) => {
     const reader = new FileReader();
@@ -34,12 +38,11 @@ function blobToBase64(blob) {
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-
     //popup의 record버튼 클릭 시 메시지 교환
     if (request.micRedord && totalRecorder){
-
       //youtube on/off
       document.getElementsByClassName('ytp-play-button ytp-button')[0].click()
+      
       if(request.micRedord==='recording start'){
         totalChunks = [];
         totalRecorder.start(10);
@@ -50,53 +53,53 @@ chrome.runtime.onMessage.addListener(
         totalBlob = new Blob(totalChunks, { 'type' : 'audio/flac' });
         var totalBlobString = blobToBase64(totalBlob);
         totalBlobString.then((result)=>{
+          console.log(result)
           chrome.storage.local.set({recordedAudio: result},()=>{
             sendResponse({recording:'success'})
           })
         }
         )
       } 
-      
     }
     //마이크 장치 권한
     else if(request.getDevices){
-          navigator.mediaDevices.enumerateDevices()
-            .then(function(devices) {
-              console.log(devices)
-              sendResponse({
-                devices: devices
-              });
-            })
-            .catch(function(err) {
-              sendResponse({
-                devices: false
-              });
-            });
-      
-    }
-
-    else if(request.getStream){
-      console.log(request.getStream)
-      navigator.mediaDevices.getUserMedia({audio:true,  deviceId:{exact: request.getStream}})
+      navigator.mediaDevices.getUserMedia({audio:{deviceId:{exact: request.getStream}, echoCancellation:false}})
       .then(function(micStream) {
-          let audioStream = document.querySelectorAll('video.video-stream ')[0].captureStream()
-          
-          let audio_voice = audioContext.createMediaStreamSource(micStream);
-          let audio_inst = audioContext.createMediaStreamSource(audioStream);
 
-          let mixedDest = audioContext.createMediaStreamDestination();
+        let audioStream = document.querySelectorAll('video.video-stream')[0].captureStream();
+        let audio_voice = audioContext.createMediaStreamSource(micStream);
+        let audio_inst = audioContext.createMediaStreamSource(audioStream);
 
-          audio_voice.connect(mixedDest)
-          audio_inst.connect(mixedDest)
+        audio_voice.connect(voiceNode);
+        audio_inst.connect(instNode);
 
-          totalRecorder =  new MediaRecorder(mixedDest.stream);
-          window.totalChunks = [];
-          totalRecorder.ondataavailable = e => {
-            totalChunks.push(e.data)
-          }
-    
-    })
-  }
+        let mixedDest = audioContext.createMediaStreamDestination();
+
+        voiceNode.connect(mixedDest)
+        instNode.connect(mixedDest)
+
+        totalRecorder =  new MediaRecorder(mixedDest.stream);
+        window.totalChunks = [];
+        totalRecorder.ondataavailable = e => {
+          totalChunks.push(e.data)
+        }
+        navigator.mediaDevices.enumerateDevices()
+          .then(function(devices) {
+            console.log(devices)
+            sendResponse({
+              devices: devices
+            });
+          })
+          .catch(function(err) {
+            sendResponse({
+              devices: false
+            });
+          });
+        });
+    }
+    else if(request.inst||request.voice){
+      request.inst ? instNode.gain.value = request.inst : voiceNode.gain.value = request.voice;
+    }
     //addListner내부 함수는 return true미 반환시 미작동
     return true
 })
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState, useEffect } from 'react'
-import logo from './logo.svg'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
-import yt from '../public/yt.png'
+import yt from '../icons/yt.svg'
+import record from'../icons/record.svg'
+import remove from'../icons/delete.svg'
+import stop from'../icons/stop.svg'
 
 const Container = ({children, color}) =>{
     return <div style={{backgroundColor:color}} id='container'>{children}</div>
 }
-const Mic = ({mic}) =>{
+
+const Mic = ({devices, setDevices}) =>{
+
     const [micDevice, setMicDevice] = useState('default');
 
     const onChange = (e) =>{
@@ -14,15 +18,28 @@ const Mic = ({mic}) =>{
     }
 
     useEffect(() => {
-        if(mic.length!=0){
+        console.log(micDevice);
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id,
                 {
-                    getStream: micDevice
-                })
-        }
-    }, [micDevice]);
+                  getDevices:micDevice
+                },
+                function(response) {
+                    console.log(response)
+                    if(response.devices){
+                        let deviceArray = response.devices.filter(x=>x.kind=='audioinput');
+                        if(deviceArray.length>=1){
+                            setDevices({ready:true, device:response.devices.filter(x=>x.kind=='audioinput')})
+                        }
+                    }
+                    else{
+                        setDevices({ready: false, device:[]})
+                    }
+                }
+            )
+        })}, [micDevice]);
 
-    if(mic.length==0){
+    if(!devices.ready&&devices.device.length==0){
         return(
             <div className='innerContainer'>
                 <p className='state' style={{color:'coral'}}>🎙 Device not found</p>
@@ -36,7 +53,7 @@ const Mic = ({mic}) =>{
                 <p className='state'>🎙Device on</p>
                 <div className='message'>
                     <select  value={micDevice} onChange={onChange}>
-                    {mic.map(device=><option value={device.deviceId}>{device.label}</option>)}
+                    {devices.device.map(device=><option value={device.deviceId}>{device.label}</option>)}
                     </select>
                 </div>
             </div>
@@ -53,36 +70,147 @@ const Sound = ({sound}) =>{
     )
 }
 
-const Record = ()=>{
-    return(
-        <div id='recordContainer'></div>
-    )
-}
-function App() {
+const Record = ({sound, devices})=>{
+    
+    //ready, notReady, recording, recorded
+    const [playingState, setPlayingState] = useState('ready');
+    const [volume, setVolume] = useState({inst:50, voice:50});
+    const audioRef = useRef();
 
-    const [devices, setDevices] = useState({ready: false, device:[]});
-    const [sound, setSound] = useState({ready: false, message:''});
+    const volumeChange = (e) =>{
+        const {value, name} = e.target
+        console.log(value)
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            if(name=='inst'){
+                chrome.tabs.sendMessage(tabs[0].id,
+                    {
+                    inst:value
+                    })
+            }else{
+                chrome.tabs.sendMessage(tabs[0].id,
+                    {
+                      voice:value
+                    })
+            }
+            })
+        setVolume({
+            ...volume,
+            [name]:value
+        })
+    }
+    const onClick = (e) =>{
+        console.log(playingState)
+        if(playingState=='ready'){
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                
+                setTimeout(function(){
+                    chrome.tabs.sendMessage(tabs[0].id,
+                        {
+                        micRedord:'recording start'
+                        }    
+                    )
+                    setPlayingState('recording');
+                },100)
+            })
+        }
+        else if(playingState=='recording'){
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id,
+                    {
+                      micRedord:'recording stop'
+                    },function(response){
+                        setTimeout(() => {
+                        response.recording =="success" ? setPlayingState('recorded'):setPlayingState('ready');
+                        }, 500);
+                    }
+                )
+            })
+        }
+        else if(playingState=='recorded'){
+            chrome.storage.local.clear(()=> console.log('localstorage cleared'))
+            setTimeout(() => {
+                setPlayingState('ready');
+            }, 3000);
+        }
+    }
 
     useEffect(() => {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id,
-                {
-                  getDevices:'getDevices'
-                },
-                function(response) {
-                    console.log(response)
-                    if(response.devices){
-                        let deviceArray = response.devices.filter(x=>x.kind=='audioinput');
-                        if(deviceArray.length>=1){
-                            setDevices({ready:true, device:response.devices.filter(x=>x.kind=='audioinput')})
-                        }
-                    }
-                    else{
-                        setDevices({ready: false, device:[]})
-                    }
+        if(playingState=='ready'){
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                if(tabs[0].audible){
+                    setPlayingState('notReady');
                 }
-            )
-        })
+            })
+        }
+        else if(playingState=='recorded'){
+            chrome.storage.local.get(['recordedAudio'],(result)=>{
+                console.log(result)
+                audioRef.current.src = result.recordedAudio;
+            })
+        }   
+    }, [playingState]);
+
+    if((sound.ready&&devices.ready)&&(playingState=='ready')){
+        return(
+            <div id='recordContainer'>
+                <p className='state'>💿 Recording</p>
+                <div className='message'>
+                    <div id='btn'>
+                        <button onClick={onClick}><img src={record}/></button>
+                    </div>
+                    <div id='vol'>
+                        <div><p>Instrument</p><input min={0} max={1} step={0.01} value={volume.inst} onChange={volumeChange} type="range" name='inst' /></div>
+                        <div><p>Voice</p><input min={0} max={1} step={0.01} value={volume.voice} onChange={volumeChange} type="range" name='voice'/></div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else if((sound.ready&&devices.ready)&&(playingState=="recording")){
+        return(
+            <div id='recordContainer'>
+                <p className='state' style={{color:'gray'}}>💿 Recording...</p>
+                <div className='message'>
+                    <div id='btn'>
+                        <button onClick={onClick}><img src={stop}/></button>
+                    </div>
+                    <div id='vol'>
+                        <pre>녹음 중....</pre>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else if((sound.ready&&devices.ready)&&(playingState=="recorded")){
+        return(
+            <div id='recordContainer'>
+                <p className='state' style={{color:'lightgreen'}}>💿 Recording Complete!!</p>
+                <div className='message'>
+                    <div id='btn'>
+                        <button onClick={onClick}><img src={remove}/></button>
+                    </div>
+                    <div id='vol'>
+                        <audio controls ref={audioRef}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else{
+        return( 
+            <div id='recordContainer'>
+                <p className='state' style={{color:'coral'}}>💿 Recording</p>
+                <p className='message'>장치 권한 혹은 정지된 영상이 필요합니다.</p>
+            </div>
+    )}
+}
+
+function App() {
+
+    const [sound, setSound] = useState({ready: false, message:''});
+    const [devices, setDevices] = useState({ready: false, device:[]});
+
+    useEffect(() => {
         chrome.tabs.query({active: true,currentWindow: true}, function (tabs) {
           if (tabs[0].url.split('v=')[0]=='https://www.youtube.com/watch?'){
               setSound({ready:true, message:tabs[0].title})
@@ -92,16 +220,16 @@ function App() {
           }
         })
     return () => {
-    };
-  }, []);
+        };
+    }, []);
 
-  return (
-    <div className="App">
-        <header><img src={yt} alt="" /><p>Recorder</p></header>
-        {sound.ready ?<Container><Sound sound={sound}></Sound><Mic mic={devices.device}></Mic><Record></Record></Container>:<Container color={'lightcoral'}>{'❌  '+ sound.message}</Container>}
-        <footer>Copyright 2022 <a target="_blank" href="https://github.com/gomiseki"> @gomiseki</a></footer>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header><img src={yt} id={'logo'}/><p>Recorder</p></header>
+            {sound.ready ?<Container><Sound sound={sound}/><Mic devices={devices} setDevices={setDevices}/><Record sound={sound} devices={devices}/></Container>:<Container color={'lightcoral'}>{'❌  '+ sound.message}</Container>}
+            <footer>Copyright 2022 <a target="_blank" href="https://github.com/gomiseki"> @gomiseki</a></footer>
+        </div>
+    )
 }
 
 export default App
